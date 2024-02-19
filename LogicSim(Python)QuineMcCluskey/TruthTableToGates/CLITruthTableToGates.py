b5df3dfb3dff31c75adc43b28bb8366dfcc7b0c0
regtoga <91752566+regtoga@users.noreply.github.com>
2024-02-19 15:34:35 -0700

    FirstOfManyLegendaryCommits

--- LogicSim(Python)QuineMcCluskey/TruthTableToGates/CLITruthTableToGates.py
+++ LogicSim(Python)QuineMcCluskey/TruthTableToGates/CLITruthTableToGates.py
@@ -17,7 +17,7 @@ import TruthTableToGatesThinker as Thinker
 
 #test cases
 TtoG = Thinker.TruthTableToGates("F(A,B,C) = Z'm(2,3,4,5)+Z'd(6,7)") 
-
+print("\nTestCase 1: ")
 print(f"TruthTable      : {TtoG.get_TruthTable()}")
 print(f"Number of Inputs: {TtoG.get_NumInputVars()}")
 print(f"Min or Max      : {TtoG.get_MinorMax()}")
@@ -25,8 +25,8 @@ print(f"minterms        : {TtoG.get_Minterms()}")
 print(f"Maxterms        : {TtoG.get_Maxterms()}")
 print(f"Dont Cares      : {TtoG.get_DontCares()}")
 
-input()
-print("")
+input("press Enter: ")
+print("\nTestCase 2: ")
 TtoG2 = Thinker.TruthTableToGates("F() = Z'M(0,1,3,5)+Z'd(2)") 
 
 print(f"TruthTable      : {TtoG2.get_TruthTable()}")
@@ -36,8 +36,8 @@ print(f"minterms        : {TtoG2.get_Minterms()}")
 print(f"Maxterms        : {TtoG2.get_Maxterms()}")
 print(f"Dont Cares      : {TtoG2.get_DontCares()}")
 
-input()
-print("")
+input("press Enter: ")
+print("\nTestCase 3: ")
 TtoG3 = Thinker.TruthTableToGates("F(A,B,C) = Z'M(1)+Z'd(6,7,100)") 
 
 print(f"TruthTable      : {TtoG3.get_TruthTable()}")
@@ -47,8 +47,8 @@ print(f"minterms        : {TtoG3.get_Minterms()}")
 print(f"Maxterms        : {TtoG3.get_Maxterms()}")
 print(f"Dont Cares      : {TtoG3.get_DontCares()}")
 
-input()
-print("")
+input("press Enter: ")
+print("\nTestCase 4: ")
 TtoG4 = Thinker.TruthTableToGates("F(A,B,C,D,E) = Z'm(1,23,101)") 
 
 print(f"TruthTable      : {TtoG4.get_TruthTable()}")
@@ -58,8 +58,8 @@ print(f"minterms        : {TtoG4.get_Minterms()}")
 print(f"Maxterms        : {TtoG4.get_Maxterms()}")
 print(f"Dont Cares      : {TtoG4.get_DontCares()}")
 
-input()
-print("")
+input("press Enter: ")
+print("\nTestCase 5: ")
 TtoG5 = Thinker.TruthTableToGates("Z'm(7)") 
 
 print(f"TruthTable      : {TtoG5.get_TruthTable()}")
