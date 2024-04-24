2a3ca701c944107f1fbcf5f69dbc66ba2a73a0b5
regtoga <91752566+regtoga@users.noreply.github.com>
2024-04-24 10:45:48 -0600

    Implemented Darkmode because i HATE lightmode

--- LogicSimPythonQuineMcCluskey/GUI_GTT_TTG_MAIN.py
+++ LogicSimPythonQuineMcCluskey/GUI_GTT_TTG_MAIN.py
@@ -8,23 +8,32 @@ import GUIsForMainGUI.TTG_gui as TTG_gui
 import GUIsForMainGUI.GTT_gui as GTT_gui
 import GUIsForMainGUI.Info_gui as Info_gui
 
+# Override tk widgets with themed ttk widgets if available
+from tkinter.ttk import *
+#pip install sv-ttk
+# ipmort sun valley theme
+import sv_ttk
+
 class MainMenu(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Main Menu")
         self.geometry("300x200")
 
-        self.btn_TTG_gui = tk.Button(self, text="TruthTable to Gates", command=self.open_TTG_gui)
+        self.btn_TTG_gui = Button(self, text="TruthTable to Gates", command=self.open_TTG_gui)
         self.btn_TTG_gui.pack(pady=10)
 
-        self.btn_GTT_gui = tk.Button(self, text="Gates to TruthTable", command=self.open_GTT_gui)
+        self.btn_GTT_gui = Button(self, text="Gates to TruthTable", command=self.open_GTT_gui)
         self.btn_GTT_gui.pack(pady=10)
 
-        self.btn_Info = tk.Button(self, text="Info", command=self.open_Info_gui)
+        self.btn_Info = Button(self, text="Info", command=self.open_Info_gui)
         self.btn_Info.pack(pady=10)
 
-        self.btn_close = tk.Button(self, text="Close", command=self.close_gui)
+        self.btn_close = Button(self, text="Close", command=self.close_gui)
         self.btn_close.pack(pady=10)
+
+        # Set the theme to light or dark
+        sv_ttk.set_theme("dark")
 
     def open_TTG_gui(self):
         self.withdraw()  # Hide the main menu
