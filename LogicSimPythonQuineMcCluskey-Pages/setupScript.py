c838510d7393dc8b3670252c184c5c3bb4b15cf3
Aidan Newberry <91752566+regtoga@users.noreply.github.com>
2024-07-13 19:34:00 -0600

    Alrighty so i have a new idea... so get this, instead of generating a while table at once, why dont i generate one bit at a time and seperate it by pages that the user can define how much shows on a page?!?

---
+++ LogicSimPythonQuineMcCluskey-Pages/setupScript.py
@@ -0,0 +1,19 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# base=”Win32GUI” should only be used with the Windows GUI app
+base = "Win32GUI" if sys.platform == "win32" else None
+
+# Dependencies are automatically detected, but may need to be adjusted.
+build_exe_options = {
+    "excludes": ["unittest"],
+    "zip_include_packages": ["encodings", "PySide6"],
+}
+
+setup(
+    name = "LogicThursdayV1.8",
+    version = "1.8",
+    description = "AidanNewberry's Basic Logic Simulation Platform",
+    options={"build_exe": build_exe_options},
+    executables = [Executable("GUI_GTT_TTG_MAIN.py", base=base)]
+)
